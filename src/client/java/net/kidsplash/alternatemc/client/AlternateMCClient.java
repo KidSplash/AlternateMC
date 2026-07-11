2e86da3b4dcb47e2501f251df39b4922c54e0a94
KidSplash <KidSplash00@gmail.com>
2026-07-11 13:21:44 -0500

    Make Mod Template.

---
+++ src/client/java/net/kidsplash/alternatemc/client/AlternateMCClient.java
@@ -0,0 +1,10 @@
+package net.kidsplash.alternatemc.client;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class AlternateMCClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
---
+++ src/client/java/net/kidsplash/alternatemc/client/AlternateMCDataGenerator.java
@@ -0,0 +1,11 @@
+package net.kidsplash.alternatemc.client;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class AlternateMCDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
