7fe8edc08e89380d8c6971029aa035cea41556b3
KidSplash <KidSplash00@gmail.com>
2026-07-11 14:19:59 -0500

    Added Orange item.

--- src/client/java/net/kidsplash/alternatemc/client/AlternateMCClient.java
+++ src/client/java/net/kidsplash/alternatemc/client/AlternateMCClient.java
@@ -1,10 +1,11 @@
 package net.kidsplash.alternatemc.client;
 
 import net.fabricmc.api.ClientModInitializer;
+import net.kidsplash.alternatemc.item.ModItems;
 
 public class AlternateMCClient implements ClientModInitializer {
 	@Override
 	public void onInitializeClient() {
-		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+		ModItems.registerModItems();
 	}
 }
---
+++ src/main/java/net/kidsplash/alternatemc/item/ModItems.java
@@ -0,0 +1,32 @@
+package net.kidsplash.alternatemc.item;
+
+import net.fabricmc.fabric.api.creativetab.v1.CreativeModeTabEvents;
+import net.kidsplash.alternatemc.AlternateMC;
+import net.minecraft.core.Registry;
+import net.minecraft.core.registries.BuiltInRegistries;
+import net.minecraft.core.registries.Registries;
+import net.minecraft.resources.Identifier;
+import net.minecraft.resources.ResourceKey;
+import net.minecraft.world.item.CreativeModeTabs;
+import net.minecraft.world.item.Item;
+
+import java.util.function.Function;
+
+public class ModItems {
+    public static final Item ORANGE = registerItem("orange", Item::new);
+
+
+    private static Item registerItem(String name, Function<Item.Properties, Item> function) {
+        return Registry.register(BuiltInRegistries.ITEM,
+                Identifier.fromNamespaceAndPath(AlternateMC.MOD_ID, name),
+                function.apply(new Item.Properties().setId(ResourceKey.create(Registries.ITEM, Identifier.fromNamespaceAndPath(AlternateMC.MOD_ID, name)))));
+    }
+
+    public static void registerModItems() {
+        AlternateMC.LOGGER.info("Registering Mod Items for " + AlternateMC.MOD_ID);
+
+        CreativeModeTabEvents.modifyOutputEvent(CreativeModeTabs.FOOD_AND_DRINKS).register(output -> {
+            output.accept(ORANGE);
+        });
+    }
+}
